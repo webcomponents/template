4f8ff3e07a349c8c0106d5b9bcac5cbea1c29edd
Steven Orvell <sorvell@google.com>
2018-03-20 16:57:06 -0700

    More correct innerHTML getter
    Fixes #32 by making `innerHTML` getter recurse so that nested templates are handled correctly.

--- template.js
+++ template.js
@@ -229,11 +229,7 @@
     var defineInnerHTML = function defineInnerHTML(obj) {
       Object.defineProperty(obj, 'innerHTML', {
         get: function() {
-          var o = '';
-          for (var e = this.content.firstChild; e; e = e.nextSibling) {
-            o += e.outerHTML || escapeData(e.data);
-          }
-          return o;
+          return getInnerHTML(this);
         },
         set: function(text) {
           contentDoc.body.innerHTML = text;
@@ -298,9 +294,11 @@
       return el;
     };
 
-    var escapeDataRegExp = /[&\u00A0<>]/g;
+    // http://www.whatwg.org/specs/web-apps/current-work/multipage/the-end.html#escapingString
+    let escapeAttrRegExp = /[&\u00A0"]/g;
+    let escapeDataRegExp = /[&\u00A0<>]/g;
 
-    var escapeReplace = function escapeReplace(c) {
+    function escapeReplace(c) {
       switch (c) {
         case '&':
           return '&amp;';
@@ -308,14 +306,113 @@
           return '&lt;';
         case '>':
           return '&gt;';
+        case '"':
+          return '&quot;';
         case '\u00A0':
           return '&nbsp;';
       }
-    };
+    }
 
-    var escapeData = function escapeData(s) {
+    function escapeAttr(s) {
+      return s.replace(escapeAttrRegExp, escapeReplace);
+    }
+
+    function escapeData(s) {
       return s.replace(escapeDataRegExp, escapeReplace);
-    };
+    }
+
+    function makeSet(arr) {
+      let set = {};
+      for (let i = 0; i < arr.length; i++) {
+        set[arr[i]] = true;
+      }
+      return set;
+    }
+
+    // http://www.whatwg.org/specs/web-apps/current-work/#void-elements
+    let voidElements = makeSet([
+      'area',
+      'base',
+      'br',
+      'col',
+      'command',
+      'embed',
+      'hr',
+      'img',
+      'input',
+      'keygen',
+      'link',
+      'meta',
+      'param',
+      'source',
+      'track',
+      'wbr'
+    ]);
+
+    let plaintextParents = makeSet([
+      'style',
+      'script',
+      'xmp',
+      'iframe',
+      'noembed',
+      'noframes',
+      'plaintext',
+      'noscript'
+    ]);
+
+    /**
+     * @param {Node} node
+     * @param {Node} parentNode
+     * @param {Function=} callback
+     */
+    function getOuterHTML(node, parentNode, callback) {
+      switch (node.nodeType) {
+        case Node.ELEMENT_NODE: {
+          let tagName = node.localName;
+          let s = '<' + tagName;
+          let attrs = node.attributes;
+          for (let i = 0, attr; (attr = attrs[i]); i++) {
+            s += ' ' + attr.name + '="' + escapeAttr(attr.value) + '"';
+          }
+          s += '>';
+          if (voidElements[tagName]) {
+            return s;
+          }
+          return s + getInnerHTML(node, callback) + '</' + tagName + '>';
+        }
+        case Node.TEXT_NODE: {
+          let data = /** @type {Text} */ (node).data;
+          if (parentNode && plaintextParents[parentNode.localName]) {
+            return data;
+          }
+          return escapeData(data);
+        }
+        case Node.COMMENT_NODE: {
+          return '<!--' + /** @type {Comment} */ (node).data + '-->';
+        }
+        default: {
+          window.console.error(node);
+          throw new Error('not implemented');
+        }
+      }
+    }
+
+    /**
+     * @param {Node} node
+     * @param {Function=} callback
+     */
+    function getInnerHTML(node, callback) {
+      if (node.localName === 'template') {
+        node =  /** @type {HTMLTemplateElement} */ (node).content;
+      }
+      let s = '';
+      let c$ = callback ? callback(node) : node.childNodes;
+      for (let i=0, l=c$.length, child; (i<l) && (child=c$[i]); i++) {
+        s += getOuterHTML(child, node, callback);
+      }
+      return s;
+    }
+
   }
 
   // make cloning/importing work!
